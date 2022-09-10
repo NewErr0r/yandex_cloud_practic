63ccdf9c06b23c50981662b9bb78604305b17266
”Dima <sechmma@gmail.com>
2022-09-10 13:17:05 +0300

    Add configure ISP

---
+++ ISP.tf
@@ -0,0 +1,33 @@
+# Create ISP
+resource "yandex_compute_instance" "isp" {
+    name        = "isp"  
+    hostname    = "isp"
+    zone        = var.zone
+
+    resources {
+        cores   = var.cores
+        memory  = var.memory
+    }
+
+    boot_disk {
+        initialize_params {
+            image_id    = var.rtr-l_image_id
+            size        = var.disk_size
+        }
+    }
+
+    network_interface {
+        subnet_id   = "e9b9iosa2umis05ld5gu"    # default-ru-central1-a
+        ip_address  = "10.128.0.100"
+        nat         = var.nat
+    }
+
+    scheduling_policy {
+        preemptible = true                            
+    }
+
+    metadata = {
+    user-data           = "${data.template_file.cloud_init_lin.rendered}"
+    serial-port-enable  = 1
+    }
+}
