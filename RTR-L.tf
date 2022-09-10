46b010c134cd4b8ba019f7bebd25c721d4921408
”Dima <sechmma@gmail.com>
2022-09-10 13:21:01 +0300

    Add configure RTR-L

---
+++ RTR-L.tf
@@ -0,0 +1,33 @@
+# Create RTR-L
+resource "yandex_compute_instance" "rtr-l" {
+    name        = "rtr-l"  
+    hostname    = "rtr-l"
+    zone        = var.zone
+
+    resources {
+        cores   = var.cores
+        memory  = var.memory
+    }
+
+    boot_disk {
+        initialize_params {
+            image_id    = var.rtr-l_image_id
+            size        = var.disk_size
+        }
+    }
+
+    network_interface {
+        subnet_id   = yandex_vpc_subnet.vnet-subnet_private_left.id
+        ip_address  = "10.0.1.100"
+        nat         = var.nat
+    }
+
+    scheduling_policy {
+        preemptible = true                            
+    }
+
+    metadata = {
+    user-data = "${data.template_file.cloud_init_lin.rendered}"
+    serial-port-enable  = 1
+    }
+}
