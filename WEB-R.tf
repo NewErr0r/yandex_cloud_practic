b965038a4fb1d9f7ee6b4b71470855a718b8e6fc
”Dima <sechmma@gmail.com>
2022-09-11 09:06:10 +0300

    Add configure WEB-R

---
+++ WEB-R.tf
@@ -0,0 +1,32 @@
+# Create WEB-R
+resource "yandex_compute_instance" "web-r" {
+    name        = "web-r"  
+    hostname    = "web-r"
+    zone        = var.zone
+
+    resources {
+        cores   = var.cores
+        memory  = var.memory
+    }
+
+    boot_disk {
+        initialize_params {
+            image_id    = var.rtr-r_image_id
+            size        = var.disk_size
+        }
+    }
+
+    network_interface {
+        subnet_id   = yandex_vpc_subnet.vnet-subnet_private_right.id
+        ip_address  = "172.16.1.200"
+    }
+
+    scheduling_policy {
+        preemptible = true                            
+    }
+
+    metadata = {
+    user-data = "${data.template_file.cloud_init_lin.rendered}"
+    serial-port-enable  = 1
+    }
+}
