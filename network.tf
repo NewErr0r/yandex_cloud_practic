e06042edc7dd8d4ab1b98856c97149dc99c20555
”Dima <sechmma@gmail.com>
2022-09-12 06:54:17 +0300

    Add configure route table

--- network.tf
+++ network.tf
@@ -8,6 +8,7 @@ resource "yandex_vpc_subnet" "vnet-subnet_private_left" {
     name            = "vnet-subnet_private_left"  
     zone            = "ru-central1-a"
     network_id      = yandex_vpc_network.vnet-left.id
+    route_table_id  = "${yandex_vpc_route_table.route-left.id}"
     v4_cidr_blocks  = ["10.0.1.0/24"]
 }
 
@@ -21,5 +22,6 @@ resource "yandex_vpc_subnet" "vnet-subnet_private_right" {
     name            = "vnet-subnet_private_right"  
     zone            = "ru-central1-a"
     network_id      = yandex_vpc_network.vnet-right.id
+    route_table_id  = "${yandex_vpc_route_table.route-right.id}"
     v4_cidr_blocks  = ["172.16.1.0/24"]
 }
